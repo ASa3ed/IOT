2d31910fe4a2f67845be9887c3958f39ef459781
ASa3ed <35202018+ASa3ed@users.noreply.github.com>
2020-02-01 22:37:32 +0200

    Update credentials.h

--- credentials/credentials.h
+++ credentials/credentials.h
@@ -1,7 +1,7 @@
 
 
-#define mySSID "     "  // ssid of your accesspoint
-#define myPASSWORD "     "  // password of access point
+#define mySSID "Hivespace1"  // ssid of your accesspoint
+#define myPASSWORD "HiveSpace2019"  // password of access point
 
 
 
